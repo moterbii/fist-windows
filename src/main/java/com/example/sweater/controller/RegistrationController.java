d46967008b5c060414537d86cfe7baaf6230ed1e
Evgeny Markin <moterbii@gmail.com>
2021-07-07 15:13:36 +0300

    Freemarker

--- src/main/java/com/example/sweater/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -20,16 +20,20 @@ public class RegistrationController {
     public String registration(){
         return "registration";
     }
+
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object > model){
         User userFromDb = userRepo.findByUsername(user.getUsername());
+
         if (userFromDb != null){
             model.put("message", "User exists");
             return "registration";
         }
+
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         userRepo.save(user);
+
         return "redirect:/login";
     }
 }
--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,11 +1,8 @@
 package com.example.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity
+@Entity// сущность которую неоходимо сохранять в базе данных
 public class Message {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
@@ -14,14 +11,30 @@ public class Message {
     private String text;
     private String tag;
 
+    @ManyToOne(fetch = FetchType.EAGER) //однмоу пользователю соответсвует множество сообщений.
+    @JoinColumn(name = "user_id")
+    private User author;
+
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
         this.text = text;
         this.tag = tag;
+        this.author = user;
     }
 
+    public String getAuthorName(){
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
     public Integer getId() {
         return id;
     }
